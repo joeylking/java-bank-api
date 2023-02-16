cca4734f5e571c81ba4994e26cf6581e484a7107
Joey King <joey.king@icloud.com>
2023-02-16 10:44:04 -0600

    Test for adding new account

--- src/test/java/com/galvanize/apibank/BankAccountEndpointTests.java
+++ src/test/java/com/galvanize/apibank/BankAccountEndpointTests.java
@@ -3,6 +3,7 @@ package com.galvanize.apibank;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,5 +22,19 @@ public class BankAccountEndpointTests {
                 .andExpect(jsonPath("$.id").value(3))
                 .andExpect(jsonPath("$.name").value("Zach"))
                 .andExpect(jsonPath("$.balance").value(9.99));
+    }
+
+    @Test
+    void addNewBankAccount() throws Exception{
+        String json = "{ \"id\": \"4\", \"name\": \"Joey\", \"balance\": \"0.99\" }";
+        mockMvc.perform(post("/api/bankaccount/add").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andDo(print())
+                .andExpect(status().isCreated());
+        mockMvc.perform(get("/api/bankaccount/4"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(4))
+                .andExpect(jsonPath("$.name").value("Joey"))
+                .andExpect(jsonPath("$.balance").value(0.99));
+
     }
 }
