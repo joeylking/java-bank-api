d757d7f2d05c6ee26e83d389944b3e08d74f02a7
Joey King <joey.king@icloud.com>
2023-02-16 09:53:42 -0600

    initialization

---
+++ src/test/java/com/galvanize/apibank/ApiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.apibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
